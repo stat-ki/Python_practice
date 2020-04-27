d893849e295c5ea616012d9feac11981590ae337
stat-ki <per_ik@outlook.com>
2020-04-27 13:30:04 +0900

    Create ex1

---
+++ exercise/ex1.py
@@ -0,0 +1,16 @@
+# Caluclate square root of x
+x = 2
+rnew = x
+# Run three times
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2)/2
+print(r1, rnew, r2)
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2)/2
+print(r1, rnew, r2)
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2)/2
+print(r1, rnew, r2)
